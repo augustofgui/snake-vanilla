fdc0757ec5fc0daf9f9586c9f343d6005e393167
Augusto Ferreira Guilarducci <augustofgui@gmail.com>
2022-01-17 01:11:09 -0300

    Game boy animation setup

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ gamePad.forEach(element => {
 var text = 40;
 var size = 10;
 
+var isIntro = true;
+
 var IPixelU = new FontFace('I Pixel U', 'url(./resources/I-pixel-u.ttf)');
 
 IPixelU.load().then(function(font) {
@@ -27,17 +29,40 @@ IPixelU.load().then(function(font) {
 });
 
 function clicked() {
-    ctx.beginPath();
     ctx.font = '48px I Pixel U';
     ctx.fillStyle = "white";
-    ctx.fillText("Clickou!", 20, text);
+    ctx.textAlign = "left";
+    ctx.fillText("Clickou!", 10, text);
     text += 40;
-    ctx.closePath();
+    if(text>canvas.height)
+        text = 40;
 }
 
-function init(){
-    drawStartingScreen();
-    setInterval(drawStartingScreen, 1000/15);
+const FRAMES_PER_SECOND = 15;  // Valid values are 60,30,20,15,10...
+// set the mim time to render the next frame
+const FRAME_MIN_TIME = (1000/60) * (60 / FRAMES_PER_SECOND) - (1000/60) * 0.5;
+var lastFrameTime = 0;  // the last frame time
+
+function update(time){
+    if(time-lastFrameTime < FRAME_MIN_TIME){ //skip the frame if the call is too early
+        requestAnimationFrame(update);
+        return; // return as there is nothing to do
+    }
+    lastFrameTime = time; // remember the time of the rendered frame
+    // render the frame
+    clearCanvas();
+    frame = decideFrame();
+    frame();
+
+    requestAnimationFrame(update); // get next farme
+}
+requestAnimationFrame(update); // start animation
+
+function decideFrame() {
+    if(isIntro)
+        return drawStartingScreen;
+    
+    return clearCanvas;
 }
 
 function clearCanvas() {
@@ -45,23 +70,16 @@ function clearCanvas() {
 }
 
 function drawStartingScreen() {
-    clearCanvas();
-
-    ctx.beginPath();
     ctx.font = size + 'px I Pixel U';
     ctx.fillStyle = "white";
     ctx.textAlign = "center"; 
     ctx.fillText("JSTATION", canvas.width/2, canvas.height/4 + size);
-    ctx.closePath();
 
-    size += 15;
+    if(size < 50)
+        size += 1;
+
+    
 }
-
-
-function draw() {
-
-}
-
-init(); 
+s
 
 
